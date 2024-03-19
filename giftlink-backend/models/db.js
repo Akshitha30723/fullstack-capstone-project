02f6c09bb071c735b85d46470e6e2fb52cd8ce47
Akshitha <44433079+Akshitha30723@users.noreply.github.com>
2024-03-18 23:17:17 -0400

    auth commit

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -7,7 +7,23 @@ let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
 const dbName = "giftdb";
+const db = await connectToDatabase();
+const collection = db.collection("users");
+const existingEmail = await collection.findOne({ email: req.body.email });
+const newUser = await collection.insertOne({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: hash,
+    createdAt: new Date(),
+});
+const payload = {
+    user: {
+        id: newUser.insertedId,
+    },
+};
 
+const authtoken = jwt.sign(payload, JWT_SECRET);
 async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
